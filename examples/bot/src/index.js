f56e180795b936acd1a2ff08c0bf6823a5a7d32a
zaida04 <nico.03727+github@gmail.com>
2022-04-27 19:00:18 -0400

    feat: embed example

--- examples/bot/src/index.js
+++ examples/bot/src/index.js
@@ -1,8 +1,9 @@
 require("dotenv/config");
 
-const { Client } = require("guilded.js");
+const { Client, Embed } = require("guilded.js");
 const client = new Client({ token: process.env.TOKEN });
 const prefix = process.env.PREFIX;
+const test_image = "https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_960_720.png";
 
 client.on("memberJoined", console.log);
 client.on("memberRemoved", console.log);
@@ -14,6 +15,26 @@ client.on("messageCreated", async (m) => {
     switch (commandName) {
         case "test": {
             await m.send("TESTING!");
+            break;
+        }
+        case "embed": {
+            await m.send(
+                new Embed()
+                    .setTitle("This is a test title!")
+                    .setDescription("This is a test description")
+                    .setColor("GREEN")
+                    .setFooter("This is a test footer", "https://google.com/")
+                    .setImage(test_image)
+                    .setThumbnail(test_image)
+                    .setTimestamp()
+                    .setURL("https://google.com")
+                    .setAuthor("Test author", test_image, "https://google.com")
+                    .addFields([
+                        { name: "Test field 1", value: "This is a test field", inline: true },
+                        { name: "Test field 2", value: "This is a test field", inline: true },
+                        { name: "Test field 3", value: "This is a test field", inline: false },
+                    ]),
+            );
             break;
         }
         case "echo": {
